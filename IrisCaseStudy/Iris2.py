cc4968c74ffe48217ae32d4eddb9b5bf793e5b2b
Kartik -KK <109861395+Kartik200214@users.noreply.github.com>
2023-01-19 00:07:26 +0530

    Add files via upload

---
+++ IrisCaseStudy/Iris2.py
@@ -0,0 +1,44 @@
+
+# Consider below characteristics of Machine Learning Application:
+
+# Classifier :       Decision Tree
+# Dataset :          Iris Dataset
+# Features :         Sepal length,Sepal width,petal length,petal width.
+# Lables  :          Versicolour,Setosa,Virginica
+# Training Dataset : 147 Entries
+# Testing Dataset :  3 Entry
+
+import numpy as np
+from sklearn import tree
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+
+print("Feature names of iris data set")
+print(iris.feature_names)
+
+print("Target names of iris data set")
+print(iris.target_names)
+
+# Indices of removed elements
+test_index = [1,51,101] 
+
+# Training data with removed elements
+train_target = np.delete(iris.target,test_index)
+train_data = np.delete(iris.data,test_index,axis= 0)
+
+# Training data for testing on trainning data
+test_target = iris.target[test_index]
+test_data = iris.data[test_index]
+
+#form decisin tree classifier
+classifier = tree.DecisionTreeClassifier()
+
+# Apply training data to form tree
+classifier.fit(train_data,train_target)
+
+print("Values that we removed for testing")
+print(test_target)
+
+print("Result to testing")
+print(classifier.predict(test_data))
